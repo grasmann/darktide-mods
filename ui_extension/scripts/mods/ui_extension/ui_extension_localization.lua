a32eea474fdc1e9045d384497d27126ae17ef0c6
xsSplater <xssplater@gmail.com>
2023-03-28 19:18:15 +0300

    Update ui_extension_localization.lua
    Russian localization.

--- ui_extension/scripts/mods/ui_extension/ui_extension_localization.lua
+++ ui_extension/scripts/mods/ui_extension/ui_extension_localization.lua
@@ -5,9 +5,11 @@ return {
 	mod_description = {
 		en = "Extends UI functionality",
 		de = "Erweitert UI Funktionalität",
+		ru = "Расширяет функциональность интерфейса",
 	},
 	bigger_item_icons = {
 		en = "Bigger Item Icons",
 		de = "Größere Item Bilder",
+		ru = "Увеличенные иконки предметов",
 	}
 }
