37711f4bef272354e614ea75537fc04d43b62252
grasmann <grasmann1805@gmail.com>
2023-04-03 22:59:34 +0200

    Update scoreboard_example_plugin.lua

--- scoreboard_example_plugin/scripts/mods/scoreboard_example_plugin/scoreboard_example_plugin.lua
+++ scoreboard_example_plugin/scripts/mods/scoreboard_example_plugin/scoreboard_example_plugin.lua
@@ -1,24 +1,24 @@
 local mod = get_mod("scoreboard_example_plugin")
+-- Get scoreboard reference
 local scoreboard = get_mod("scoreboard")
-local DMF = get_mod("DMF")
 
+-- Hook jumping state
 mod:hook(CLASS.PlayerCharacterStateJumping, "on_enter", function(func, self, unit, ...)
     func(self, unit, ...)
+    -- Check scoreboard; could not be installed
     if scoreboard then
         local player = scoreboard:player_from_unit(unit)
         if player then
+            -- Get account id from player
+            -- Bots don't have account ids; use name instead
             local account_id = player:account_id() or player:name()
+            -- Update stat
             scoreboard:update_stat("scoreboard_example", account_id, 1)
         end
     end
 end)
 
---DMFOptionsView.cb_on_category_pressed = function (self, widget, entry)
-mod:hook(CLASS.DMFOptionsView, "on_enter", function(func, self, ...)
-    func(self, ...)
-    mod:echo("lol")
-end)
-
+-- Define rows
 mod.scoreboard_rows = {
     {name = "scoreboard_example",
 		text = "row_scoreboard_example",
